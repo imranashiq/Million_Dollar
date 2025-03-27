1d4f53499dbcf4401123c40cbf695b349f98be0b
Talha Khalid <hafizmtalhakhalid@gmail.com>
2025-03-27 15:39:49 +0500

    CORS origin updates

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://reimagined-couscous-drab.vercel.app"], // Your frontend domain
+  origin: [
+    "http://localhost:5173",
+    "https://reimagined-couscous-drab.vercel.app",
+    "https://milliondollarinfluencer.com",
+    "https://milliondollarinfluencer.com/",
+  ], // Your frontend domain
   methods: "GET,POST,PUT,DELETE,OPTIONS,PATCH",
   allowedHeaders: "Content-Type,Authorization",
   credentials: true, // Enable if using cookies/tokens
@@ -23,20 +28,18 @@ const PORT = process.env.PORT;
 const authRouter = require("./routes/auth");
 const influencerRouter = require("./routes/influencer");
 const pixelRouter = require("./routes/pixels");
-const marketPlaceRouter=require("./routes/marketplace")
-
+const marketPlaceRouter = require("./routes/marketplace");
 
 app.get("/", (req, res) => {
   res.send(
     "<h1 style='display: flex; justify-content: center;  align-items: center; height: 200px'>Welcome to Million Dollar Backend</h1>"
   );
 });
-pixelRouter
+pixelRouter;
 app.use("/api/v1", authRouter);
 app.use("/api/v1", influencerRouter);
 app.use("/api/v1", pixelRouter);
 app.use("/api/v1", marketPlaceRouter);
-
 
 const start = async () => {
   try {
