8b6dc10ebeeef993a2d734359a3f170430b7d8b1
Imran Ashiq <131940532+imranashiq@users.noreply.github.com>
2025-04-06 18:43:25 +0500

    get

--- routes/marketplace.js
+++ routes/marketplace.js
@@ -7,7 +7,7 @@ const  upload  = require("../utills/upload");
 marketPlaceRouter.post("/createMarketPlace",userAuth, createMarketPlace);
 marketPlaceRouter.post("/createBid",userAuth, createBid);
 marketPlaceRouter.get("/getAllMarketPlace", getAllMarketPlace);
-marketPlaceRouter.patch("/getAllBids",userAuth, getAllBids);
+marketPlaceRouter.get("/getAllBids",userAuth, getAllBids);
 
 
 
